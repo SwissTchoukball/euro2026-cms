ad0bc04fb7627c79f00b2b6739cf2755f2ccc50d
David Sandoz <david.sandoz@tchoukball.ch>
2025-11-05 17:27:42 +0100

    feat: enable headless mode

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,15 @@
-<h1><?= $page->title() ?></h1>
+<?php
+
+/** @var \Kirby\Cms\Page $page */
+
+$data = [
+  '__meta' => [
+    'template' => $page->intendedTemplate()->name(),
+    'isHomePage' => $page->isHomePage(),
+    'isErrorPage' => $page->isErrorPage()
+  ],
+  'title' => $page->title()->value(),
+  'text' => $page->text()->kirbytext()->value(),
+];
+
+echo \Kirby\Data\Json::encode($data);
