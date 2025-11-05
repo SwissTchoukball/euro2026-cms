d9904d9aaa86b4adaa405e5c353e310ee3fb0d12
David Sandoz <david.sandoz@tchoukball.ch>
2025-11-05 15:48:40 +0100

    fix: remove duplicate declaration of content folder

--- public/index.php
+++ public/index.php
@@ -6,7 +6,6 @@ $kirby = new Kirby([
     'roots' => [
         'index'    => __DIR__,
         'base'     => $base = dirname(__DIR__),
-        'content'  => $base . '/content',
         'site'     => $base . '/site',
         'storage'  => $storage = $base . '/storage',
         'accounts' => $storage . '/accounts',
